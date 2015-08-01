2130be424089693cb48ceebb27c03ef2ec5b38b4
Eric Wing <ewing -dot- public |-at-| gmail.com>
2015-08-01 04:50:12 -0700

    Cocoa: Initial text field/view scaffolding

--- src/cocoa/iupcocoa_text.m
+++ src/cocoa/iupcocoa_text.m
@@ -31,6 +31,8 @@
 #include "iupcocoa_drv.h"
 
 
+
+
 void iupdrvTextAddSpin(int *w, int h)
 {
 
@@ -77,11 +79,174 @@ void iupdrvTextAddFormatTag(Ihandle* ih, Ihandle* formattag, int bulk)
 }
 
 
+
+
+static int cocoaTextMapMethod(Ihandle* ih)
+{
+	NSView* the_view;
+	
+
+	
+	if (ih->data->is_multiline)
+	{
+//		NSTextView* text_view = [[NSTextView alloc] initWithFrame:NSZeroRect];
+		NSTextView* text_view = [[NSTextView alloc] initWithFrame:NSMakeRect(0, 0, 400, 400)];
+		the_view = text_view;
+		
+
+		int wordwrap = 0;
+		
+
+
+		
+		/* formatting is always supported when MULTILINE=YES */
+		ih->data->has_formatting = 1;
+		
+		if (iupAttribGetBoolean(ih, "WORDWRAP"))
+		{
+			wordwrap = 1;
+			ih->data->sb &= ~IUP_SB_HORIZ;  /* must remove the horizontal scroolbar */
+			
+			
+
+		}
+		else
+		{
+			NSSize layout_size = [text_view maxSize];
+			layout_size.width = layout_size.height;
+			[text_view setMaxSize:layout_size];
+			[[text_view textContainer] setWidthTracksTextView:NO];
+			[[text_view textContainer] setContainerSize:layout_size];
+			
+		}
+		
+	}
+	else
+	{
+		NSTextField* text_field;
+		
+		
+		// IMPORTANT: Secure text fields are not togglable in Cocoa
+		// It might be fakeable, however, since this is security related, mucking with it is ill-advised.
+		// Also Mac App Store may reject ill-advised things.
+		if(iupAttribGetBoolean(ih, "PASSWORD"))
+		{
+
+			//text_field = [[NSSecureTextField alloc] initWithFrame:NSZeroRect];
+			text_field = [[NSSecureTextField alloc] initWithFrame:NSMakeRect(0, 0, 140, 40)];
+			
+		}
+		else
+		{
+			
+			//text_field = [[NSTextField alloc] initWithFrame:NSZeroRect];
+			text_field = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 140, 40)];
+			
+			
+		}
+		the_view = text_field;
+
+
+		if(iupAttribGetBoolean(ih, "SPIN"))
+		{
+			// TODO: NSStepper
+			
+			/*
+			gtk_spin_button_set_numeric((GtkSpinButton*)ih->handle, FALSE);
+			gtk_spin_button_set_digits((GtkSpinButton*)ih->handle, 0);
+			
+			gtk_spin_button_set_wrap((GtkSpinButton*)ih->handle, iupAttribGetBoolean(ih, "SPINWRAP"));
+			
+			g_signal_connect(G_OBJECT(ih->handle), "value-changed", G_CALLBACK(gtkTextSpinValueChanged), ih);
+			g_signal_connect(G_OBJECT(ih->handle), "output", G_CALLBACK(gtkTextSpinOutput), ih);
+			
+			if (!iupAttribGetBoolean(ih, "SPINAUTO"))
+			{
+				g_signal_connect(G_OBJECT(ih->handle), "input", G_CALLBACK(gtkTextSpinInput), ih);
+				iupAttribSet(ih, "_IUPGTK_SPIN_NOAUTO", "1");
+			}
+			 */
+		}
+		else
+		{
+			
+		}
+		
+
+		/* formatting is never supported when MULTILINE=NO */
+		ih->data->has_formatting = 0;
+		
+
+	}
+	
+	
+	
+	[the_view sizeToFit];
+	
+	
+	
+	ih->handle = the_view;
+	
+#if 0
+	// I'm using objc_setAssociatedObject/objc_getAssociatedObject because it allows me to avoid making subclasses just to hold ivars.
+	objc_setAssociatedObject(the_toggle, IHANDLE_ASSOCIATED_OBJ_KEY, (id)ih, OBJC_ASSOCIATION_ASSIGN);
+	// I also need to track the memory of the buttion action receiver.
+	// I prefer to keep the Ihandle the actual NSView instead of the receiver because it makes the rest of the implementation easier if the handle is always an NSView (or very small set of things, e.g. NSWindow, NSView, CALayer).
+	// So with only one pointer to deal with, this means we need our Toggle to hold a reference to the receiver object.
+	// This is generally not good Cocoa as Toggles don't retain their receivers, but this seems like the best option.
+	// Be careful of retain cycles.
+	IupCocoaToggleReceiver* toggle_receiver = [[IupCocoaToggleReceiver alloc] init];
+	[the_toggle setTarget:toggle_receiver];
+	[the_toggle setAction:@selector(myToggleClickAction:)];
+	// I *think* is we use RETAIN, the object will be released automatically when the Toggle is freed.
+	// However, the fact that this is tricky and I had to look up the rules (not to mention worrying about retain cycles)
+	// makes me think I should just explicitly manage the memory so everybody is aware of what's going on.
+	objc_setAssociatedObject(the_toggle, IUP_COCOA_TOGGLE_RECEIVER_OBJ_KEY, (id)toggle_receiver, OBJC_ASSOCIATION_ASSIGN);
+	
+#endif
+	// All Cocoa views shoud call this to add the new view to the parent view.
+	iupCocoaAddToParent(ih);
+	
+	
+
+
+#if 0
+	/* configure for DRAG&DROP */
+	if (IupGetCallback(ih, "DROPFILES_CB"))
+		iupAttribSet(ih, "DROPFILESTARGET", "YES");
+	
+	/* update a mnemonic in a label if necessary */
+	iupgtkUpdateMnemonic(ih);
+	
+	if (ih->data->formattags)
+		iupTextUpdateFormatTags(ih);
+#endif
+		
+	
+	return IUP_NOERROR;
+}
+
+
+static void cocoaToggleUnMapMethod(Ihandle* ih)
+{
+	id the_view = ih->handle;
+	/*
+	id text_receiver = objc_getAssociatedObject(the_view, IUP_COCOA_TOGGLE_RECEIVER_OBJ_KEY);
+	objc_setAssociatedObject(the_view, IUP_COCOA_TOGGLE_RECEIVER_OBJ_KEY, nil, OBJC_ASSOCIATION_ASSIGN);
+	[text_receiver release];
+	*/
+	[the_view release];
+	ih->handle = NULL;
+	
+}
+
+
 void iupdrvTextInitClass(Iclass* ic)
 {
-#if 0
   /* Driver Dependent Class functions */
-  ic->Map = gtkTextMapMethod;
+  ic->Map = cocoaTextMapMethod;
+
+#if 0
 
   /* Driver Dependent Attribute functions */
 
