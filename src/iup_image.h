ab3297776d9ad16f5b66c4c36fcf220f160a05ae
Eric Wing <ewing -dot- public |-at-| gmail.com>
2017-04-15 19:12:54 -0700

    SymbolVisibility: Discovered that IUP exports some functions that are not in the public headers. This requires more annotations in a lot of other files.
    To get started, I added a new private header, iup_export.h, which
    defines IUP_EXPORTI, where the trailing I is for "internal api".
    This keeps it distinct from IUP_EXPORT which can be for fully public
    APIs.
    Additionally, I don't want to add another public header.
    And I don't want to make all these other headers include iup.h when they
    do not already. So iup_export.h is the compromise.
    
    TODO: I need to manually audit the entire export list and annotate
    everything not already needed.

--- src/iup_image.h
+++ src/iup_image.h
@@ -7,6 +7,8 @@
 #ifndef __IUP_IMAGE_H 
 #define __IUP_IMAGE_H
 
+#include "iup_export.h" /* need IUP_EXPORTI */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,15 +32,15 @@ void iupImageGetInfo(const char* name, int *w, int *h, int *bpp);
 void iupImageUpdateParent(Ihandle *parent);
 void iupImageClearFromCache(Ihandle* ih, void* handle);
 
-Ihandle* iupImageGetHandle(const char* name);
+IUP_EXPORTI Ihandle* iupImageGetHandle(const char* name);
 
 typedef struct _iupColor { 
   unsigned char r, g, b, a; 
 } iupColor;
 
-int iupImageInitColorTable(Ihandle *ih, iupColor* colors, int *colors_count);
+IUP_EXPORTI int iupImageInitColorTable(Ihandle *ih, iupColor* colors, int *colors_count);
 void iupImageInitNonBgColors(Ihandle* ih, unsigned char *colors);
-void iupImageColorMakeInactive(unsigned char *r, unsigned char *g, unsigned char *b, 
+IUP_EXPORTI void iupImageColorMakeInactive(unsigned char *r, unsigned char *g, unsigned char *b, 
                                unsigned char bg_r, unsigned char bg_g, unsigned char bg_b);
 int iupImageNormBpp(int bpp);
 
@@ -54,8 +56,8 @@ void iupdrvImageGetRawData(void* handle, unsigned char* imgdata);
 void iupImageStockInit(void);
 void iupImageStockFinish(void);
 typedef Ihandle* (*iupImageStockCreateFunc)(void);
-void iupImageStockSet(const char *name, iupImageStockCreateFunc func, const char* native_name);
-void iupImageStockLoadAll(void);  /* Used only in IupView */
+IUP_EXPORTI void iupImageStockSet(const char *name, iupImageStockCreateFunc func, const char* native_name);
+IUP_EXPORTI void iupImageStockLoadAll(void);  /* Used only in IupView */
 int iupImageStockGetSize(void);
 
 
