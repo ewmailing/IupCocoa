26f9a499924b88267cf7ba93529df466f47afa65
Eric Wing <ewing -dot- public |-at-| gmail.com>
2017-05-15 19:50:36 -0700

    Cocoa: Removed unused code from TextFieldCell.
    I originally was going to use Daniel Jalkut's implementation (MIT), but it didn't work. It seemed to only kick in when I clicked a mouse on it, which is an entirely different use case than what we are after.

--- src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.h
+++ src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.h
@@ -1,10 +1,3 @@
-//
-//  RSVerticallyCenteredTextFieldCell.h
-//  RSCommon
-//
-//  Created by Daniel Jalkut on 6/17/06.
-//  Copyright 2006 Red Sweater Software. All rights reserved.
-//
 
 #import <Cocoa/Cocoa.h>
 
--- src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.m
+++ src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.m
@@ -21,21 +21,10 @@
 	{
 		case IUPTextVerticalAlignmentCenter:
 		{
-			
-		//	CGFloat original_y = cell_frame.origin.y;
 			title_rect.origin.y = cell_frame.origin.y + (cell_frame.size.height - string_height) * 0.5;
 			
 			// There is some discussion to change the height if the text height is greater that the available height.
 			// But I think that would screw up IUP's sizing mechanisms.
-
-			/*
-			if (textRect.size.height < titleFrame.size.height) {
-				titleFrame.origin.y = theRect.origin.y + (theRect.size.height - textRect.size.height) / 2.0;
-				titleFrame.size.height = textRect.size.height;
-			}
-			
-			title_rect.size.height = title_rect.size.height - (title_rect.origin.y - original_y);
-			 */
 			
 			break;
 		}
@@ -54,11 +43,8 @@
 			break;
 		}
 	}
-	
 
 	return title_rect;
-	
-	
 }
 
 - (void) drawInteriorWithFrame:(NSRect)cell_frame inView:(NSView*)control_view
@@ -70,51 +56,5 @@
 {
 	[super selectWithFrame:[self titleRectForBounds:cell_frame] inView:control_view editor:text_obj delegate:the_delegate start:sel_start length:sel_length];
 }
-
-
-#if 0
-- (NSRect)drawingRectForBounds:(NSRect)theRect
-{
-	// Get the parent's idea of where we should draw
-	NSRect newRect = [super drawingRectForBounds:theRect];
-
-	// When the text field is being 
-	// edited or selected, we have to turn off the magic because it screws up 
-	// the configuration of the field editor.  We sneak around this by 
-	// intercepting selectWithFrame and editWithFrame and sneaking a 
-	// reduced, centered rect in at the last minute.
-	if (mIsEditingOrSelecting == NO)
-	{
-		// Get our ideal size for current text
-		NSSize textSize = [self cellSizeForBounds:theRect];
-
-		// Center that in the proposed rect
-		float heightDelta = newRect.size.height - textSize.height;	
-		if (heightDelta > 0)
-		{
-			newRect.size.height -= heightDelta;
-			newRect.origin.y += (heightDelta / 2);
-		}
-	}
-	
-	return newRect;
-}
-
-- (void)selectWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject start:(NSInteger)selStart length:(NSInteger)selLength
-{
-	aRect = [self drawingRectForBounds:aRect];
-	mIsEditingOrSelecting = YES;	
-	[super selectWithFrame:aRect inView:controlView editor:textObj delegate:anObject start:selStart length:selLength];
-	mIsEditingOrSelecting = NO;
-}
-
-- (void)editWithFrame:(NSRect)aRect inView:(NSView *)controlView editor:(NSText *)textObj delegate:(id)anObject event:(NSEvent *)theEvent
-{	
-	aRect = [self drawingRectForBounds:aRect];
-	mIsEditingOrSelecting = YES;
-	[super editWithFrame:aRect inView:controlView editor:textObj delegate:anObject event:theEvent];
-	mIsEditingOrSelecting = NO;
-}
-#endif
 
 @end
