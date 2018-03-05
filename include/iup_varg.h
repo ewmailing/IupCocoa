3c2565d34554e22914c705ba12c256887d112e20
Eric Wing <ewing -dot- public |-at-| gmail.com>
2018-03-04 18:13:08 -0800

    SymbolVisibility: iup_varg.h

--- include/iup_varg.h
+++ include/iup_varg.h
@@ -14,32 +14,32 @@
 extern "C" {
 #endif
 
-void      IupLogV(const char* type, const char* format, va_list arglist);
+IUP_EXPORT void      IupLogV(const char* type, const char* format, va_list arglist);
 
-Ihandle*  IupSetAttV(const char* handle_name, Ihandle* ih, const char* name, va_list arglist);
+IUP_EXPORT Ihandle*  IupSetAttV(const char* handle_name, Ihandle* ih, const char* name, va_list arglist);
 
-void      IupSetStrfV(Ihandle* ih, const char* name, const char* format, va_list arglist);
-void      IupSetStrfIdV(Ihandle* ih, const char* name, int id, const char* format, va_list arglist);
-void      IupSetStrfId2V(Ihandle* ih, const char* name, int lin, int col, const char* format, va_list arglist);
+IUP_EXPORT void      IupSetStrfV(Ihandle* ih, const char* name, const char* format, va_list arglist);
+IUP_EXPORT void      IupSetStrfIdV(Ihandle* ih, const char* name, int id, const char* format, va_list arglist);
+IUP_EXPORT void      IupSetStrfId2V(Ihandle* ih, const char* name, int lin, int col, const char* format, va_list arglist);
 
-Ihandle*  IupSetCallbacksV(Ihandle* ih, const char *name, Icallback func, va_list arglist);
+IUP_EXPORT Ihandle*  IupSetCallbacksV(Ihandle* ih, const char *name, Icallback func, va_list arglist);
 
-Ihandle*  IupCreateV(const char *classname, void* first, va_list arglist);
-Ihandle*  IupVboxV(Ihandle* child, va_list arglist);
-Ihandle*  IupZboxV(Ihandle* child, va_list arglist);
-Ihandle*  IupHboxV(Ihandle* child,va_list arglist);
-Ihandle*  IupNormalizerV(Ihandle* ih_first, va_list arglist);
-Ihandle*  IupCboxV(Ihandle* child, va_list arglist);
-Ihandle*  IupGridBoxV(Ihandle* child, va_list arglist);
-Ihandle*  IupMenuV(Ihandle* child,va_list arglist);
-Ihandle*  IupTabsV(Ihandle* child, va_list arglist);
-Ihandle*  IupFlatTabsV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupCreateV(const char *classname, void* first, va_list arglist);
+IUP_EXPORT Ihandle*  IupVboxV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupZboxV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupHboxV(Ihandle* child,va_list arglist);
+IUP_EXPORT Ihandle*  IupNormalizerV(Ihandle* ih_first, va_list arglist);
+IUP_EXPORT Ihandle*  IupCboxV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupGridBoxV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupMenuV(Ihandle* child,va_list arglist);
+IUP_EXPORT Ihandle*  IupTabsV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupFlatTabsV(Ihandle* child, va_list arglist);
 
-void      IupMessageV(const char *title, const char *format, va_list arglist);
-Ihandle*  IupParamBoxV(Ihandle* param, va_list arglist);
-int       IupGetParamV(const char* title, Iparamcb action, void* user_data, const char* format, va_list arglist);
+IUP_EXPORT void      IupMessageV(const char *title, const char *format, va_list arglist);
+IUP_EXPORT Ihandle*  IupParamBoxV(Ihandle* param, va_list arglist);
+IUP_EXPORT int       IupGetParamV(const char* title, Iparamcb action, void* user_data, const char* format, va_list arglist);
 
-Ihandle*  IupGLCanvasBoxV(Ihandle* child, va_list arglist);
+IUP_EXPORT Ihandle*  IupGLCanvasBoxV(Ihandle* child, va_list arglist);
 
 
 #endif
