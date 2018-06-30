4a9f8d04018db946cf556ecfc416d125a1c87439
Eric Wing <ewing -dot- public |-at-| gmail.com>
2018-06-30 12:10:02 -0700

    IupPostMessage: Cocoa: Implemented prototype IupPostMessage for Cocoa.

--- src/cocoa/iupcocoa_loop.m
+++ src/cocoa/iupcocoa_loop.m
@@ -197,3 +197,19 @@ void IupFlush(void)
 	[NSApp terminate:nil];
 }
 
+/* TODO: Make decision on final API. For now, this API is just to get a usable demo. */
+void IupPostMessage(Ihandle* ih, char* unusedchar, void* message_data, int unusedint)
+{
+	dispatch_async(dispatch_get_main_queue(),
+		^{
+			//NSLog(@"dispatch_async fired on main_queue");
+			IFnsVi post_message_callback = (IFnsVi)IupGetCallback(ih, "POSTMESSAGE_CB");
+			if (post_message_callback)
+			{
+				//post_message_callback(ih, NULL, message_data, 0);
+				post_message_callback(ih, unusedchar, message_data, unusedint);
+			}
+		}
+	);
+}
+
