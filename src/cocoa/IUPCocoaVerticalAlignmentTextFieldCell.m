9111bdf42b9c09bdc7371cd43b5e11b4a7c88c24
Eric Wing <ewing -dot- public |-at-| gmail.com>
2018-09-19 00:22:38 -0700

    Cocoa: Label: Vertical alignment bug: Pasted feedback from Apple Bug report.

--- src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.m
+++ src/cocoa/IUPCocoaVerticalAlignmentTextFieldCell.m
@@ -4,6 +4,69 @@
 // But we have to implement a more general version to deal with different vertical alignments.
 //
 
+/*
+// Update:
+Summary:
+I have been trying to create my own subclass of NSTextFieldCell that supports the notion of vertical alignment. The goal is for a layout/designer to  provide an oversized area for text to be dropped into to handle different arbitrary sentences of different/unknown lengths. And that widget will automatically vertically align as specified if there is much more space than actual text.
+
+There is pretty much one technique on the internet that everybody seems to use for this.
+
+This generally works, except when I enable setSelectable:YES on the NSTextField. Then the rendering positioning gets messed up.
+
+Typically, the initial render will start by putting the text in one location. (I think it is the right location.) But the moment I click on the window or resize or do something to trigger a redraw, the text position jumps to another location.
+
+I think the first location is the correct location, and the other location is the location that Cocoa would put it if it bypassed/ignored my subclass modifications.
+
+Steps to Reproduce:
+I have a attached an Xcode project that demonstrates the problem.
+- Build and run it.
+- See the text on screen.
+- Click the text and see it suddenly jump to a different location.
+
+Basically, I created a subclass of NSTextFieldCell and am using it. I also enabled setSelected:YES on my NSTextField.
+
+
+Expected Results:
+
+The text should be positioned where I say it should be in my Cell subclass, and should not jump between the two different locations.
+This should work with setSelected: in any configuration.
+
+See attached InitialPosition.png
+
+Actual Results:
+After the initial render, the text jumps to the location that seems to ignore my positioning changes, when setSelected:YES is picked.
+
+See attached JumpedPosition.png
+
+Version/Build:
+10.13.6 (17G65)
+Xcode  9.4.1 (9F2000)
+
+I have also seen this in 10.12 and I maybe 10.11. (I keep putting off filing this bug.)
+
+Configuration:
+I am running an iMac 20" mid 2010.
+Also reproduced on Macbook Air 11" mid 2010
+
+
+Apple Response:
+September 18 2018, 7:47 PM
+Engineering has the following feedback for you:
+
+If you are supporting selectable text fields, this is not sufficient for what you are trying to do.
+
+During selection, the field editor is rendering the text+selection,
+see: https://developer.apple.com/library/archive/documentation/TextFonts/Conceptual/CocoaTextArchitecture/TextFieldsAndViews/TextFieldsAndViews.html#//apple_ref/doc/uid/TP40009459-CH8-BBCFEBHA
+
+So you also need to provide your own field editor so that you can control its positioning to match the cell.
+You could probably use a stock NSTextView for this.
+In your NSTextFieldCell subclass override -(NSTextView *)fieldEditorForView:(NSView *)controlView and return your own NSTextView instance there.
+
+Though really, what you’re trying to do would probably be better served by setting up the app layouts with appropriate
+Auto Layout constraints that adjust the size and positioning of the text field.
+We are now closing this bug report.  If you have questions or comments about the resolution, please update your bug report with that information so we can respond.
+*/
+
 #import "IUPCocoaVerticalAlignmentTextFieldCell.h"
 
 @implementation IUPCocoaVerticalAlignmentTextFieldCell
